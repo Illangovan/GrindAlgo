da457fd8106541260c494fad1e7a05efba0e25de
Illangovan <andrewillango1212@gmail.com>
2024-12-25 09:12:11 -0500

    updated

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../styles/Home.css'
+import { Link } from 'react-router-dom'
 function Home() {
   return (
     <div className='home'>
         <div className="home_description">
-          <h1>Master DSA and System Design</h1>
-          <p>start learning dsa</p>
+          <h1>DSA and System Design Preparation Hub!</h1>
+          <p>Learn Data Structures and Algorithms systematically. Practice LeetCode problems grouped by patterns. Ace your coding interviews.</p>
+          <button><Link to='/Problems'>Start Practising</Link></button>
         </div>
     </div>
   )
