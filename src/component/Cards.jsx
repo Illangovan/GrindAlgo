65a19091023842781a70eb37802d41d82d7bca1e
Illangovan <andrewillango1212@gmail.com>
2024-12-25 13:33:42 -0500

    updated

---
+++ src/component/Cards.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/Cards.css'
+function Cards() {
+  return (
+    <>
+    <div className="card_container">
+    <h5>logo</h5>
+    <h2>heading</h2>
+    <h2>description</h2>
+    </div>
+    </>
+  )
+}
+
+export default Cards
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/Home.css'
+import Cards from './Cards'
 import { Link } from 'react-router-dom'
 function Home() {
   return (
@@ -7,7 +8,16 @@ function Home() {
         <div className="home_description">
           <h1>DSA and System Design Practice Arena!</h1>
           <p>Learn Data Structures and Algorithms systematically. Practice LeetCode problems grouped by patterns. Ace your coding interviews.</p>
-          <Link to='/Problems'><button>Start Practising</button></Link>
+          <Link to='/Problems'><button>solve problems!</button></Link>
+        </div>
+        <div className='features'>
+          <h1>Key Features</h1>
+          <div className="cards">
+          <Cards />
+          <Cards/>
+          <Cards/>
+          </div>
+          
         </div>
     </div>
   )
