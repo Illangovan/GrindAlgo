1c8d530b2caaf1246f920460795847dcc2df1fe5
Illangovan <andrewillango1212@gmail.com>
2024-12-25 20:39:42 -0500

    updated

--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../styles/Cards.css'
-function Cards() {
+import { FcInspection } from "react-icons/fc";
+function Cards(props) {
   return (
     <>
     <div className="card_container">
-    <h5>logo</h5>
+    <FcInspection size={40}/>
     <h2>heading</h2>
     <h2>description</h2>
     </div>
